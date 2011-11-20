48e70b46635cb186398c223f9f17aa7d67ec4f52
Lance <ze@Lances-MacBook-Air.local>
2011-11-19 21:56:48 -0500

    Thus far some changes I have made to some of the destination points and variables for spacing.

---
+++ level2.js
@@ -0,0 +1,22 @@
+(function(level) {
+	for (i = 0; i < level.length; i++) {
+		new Space(i, 5, level);
+	}
+
+	(new Space(1, 4, level)).source = "angel";
+	(new Space(7, 4, level)).source = "angel";
+	(new Space(8, 4, level)).source = "angel";
+	(new Space(7, 4, level)).source = "angel";
+	(new Space(6, 4, level)).source = "angel";
+	(new Space(5, 4, level)).source = "angel";
+	(new Space(4, 4, level)).source = "angel";
+	(new Space(3, 4, level)).source = "angel";
+	(new Space(2, 4, level)).source = "angel";
+	(new Space(1, 4, level)).source = "angel";
+	(new Space(0, 4, level)).source = "angel";
+
+	setDestination("gargoyle", level, 0, 5);
+	setDestination("angel", level, level.length -1, 9);
+
+})(newLevel(16, 10));
+
